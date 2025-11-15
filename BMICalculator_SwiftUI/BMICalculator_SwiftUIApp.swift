0921ffac5680b9c1618733f001b6b8e52ade68c8
ShreyasTantri <shreyastantrik29@gmail.com>
2025-11-15 15:58:01 +0530

    Initial Commit

---
+++ BMICalculator_SwiftUI/BMICalculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculator_SwiftUIApp.swift
+//  BMICalculator_SwiftUI
+//
+//  Created by CCS038 on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMICalculator_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMICalculator_SwiftUI
+//
+//  Created by CCS038 on 15/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
